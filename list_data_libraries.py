efb268035cd79a5d9f9d5bf36996170bc6ee7fa4
Madi Flannery <madisonflannery93@gmail.com>
2016-06-14 12:15:24 +1000

    Changed to use symbolic links instead of copying files

--- list_data_libraries.py
+++ list_data_libraries.py
@@ -1,37 +1,65 @@
-"""
-This example retrieves details of all the Data Libraries available to us and lists information on them.
-Usage: python list_data_libraries.py <galaxy-url> <galaxy-API-key>
-"""
+'''
+
+'''
 from __future__ import print_function
+from collections import defaultdict
+import os
 import sys
 
 from bioblend.galaxy import GalaxyInstance
 
-galaxy_url = 'http://115.146.94.125/galaxy/'
-galaxy_key = '76822b3132377362e5607732b2574766'
+GALAXY_URL = 'http://127.0.0.1/galaxy/'
+GALAXY_KEY = '76822b3132377362e5607732b2574766'
+REFSEQ_DIR = '/mnt/galaxyIndices/Bacteria/'
+GENUS = 'Acetobacter'.lower()
 
-def getLibs(gi):
-    libraries = gi.libraries.get_libraries()
-
-    if len(libraries) == 0:
-        print("There are no Data Libraries available.")
-    else:
-        print("\nData Libraries:")
-        for lib_dict in libraries:
-            print("{0} : {1}".format(lib_dict['name'], lib_dict['id']))
+if len(sys.argv) > 1:
+    #get things like URL, KEY, REFSEQ_DIR and GENUS
+    pass
 
 
+# Initiating Galaxy connection
+
+gi = GalaxyInstance(url=GALAXY_URL, key=GALAXY_KEY)
+
+# Get a dict of all RefSeq directories
+dirs = defaultdict(list)
+
+for folder in os.listdir(REFSEQ_DIR):
+    if folder[0] != ".":
+        folder_tmp = folder
+
+        if folder_tmp.find("_") == 0:
+            folder_tmp = folder_tmp[1:]
+
+        split_point = folder_tmp.find("_")
+        dirs[folder_tmp[:split_point].lower()].append(folder)
+
+if GENUS not in dirs:
+    print("There are no results for your specified genus " + GENUS)
+    SystemExit(1)
 
 
 
-print("Initiating Galaxy connection")
+# Check for existing libraries
+libraries = gi.libraries.get_libraries()
 
-gi = GalaxyInstance(url=galaxy_url, key=galaxy_key)
+# NOTE: THIS IS A DEBUGGING STEP
+# todo: change this code 
+if GENUS in [lib['name'] for lib in libraries]:
+    lib_id = gi.libraries.get_libraries(name=GENUS)[0]['id']
+    gi.libraries.delete_library(lib_id)
 
-print("Retrieving Data Library list")
+if GENUS not in libraries:
+    # Create library
+    new_lib = gi.libraries.create_library(GENUS, "Reference genomes for " + GENUS)
 
-getLibs(gi)
-
-lib = gi.libraries.create_library("test-lib")
-
-getLibs(gi)
+    # Create folders, upload files
+    for folder in dirs[GENUS]:
+        fldr = gi.libraries.create_folder(new_lib['id'], folder)[0]
+        for fna in os.listdir(REFSEQ_DIR + folder):
+            gi.libraries.upload_from_galaxy_filesystem(
+                library_id=new_lib['id'],
+                filesystem_paths=REFSEQ_DIR + folder + "/" + fna,
+                folder_id=fldr['id'],
+                link_data_only="link_to_files")
